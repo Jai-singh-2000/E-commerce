59b4fd8a6bc6c6e17b5f93cf19781b824ab04023
suraj kumar <surajgautam56878@gmail.com>
2023-04-24 02:26:13 +0530

    otp , forget password and change password modal done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Temp from './pages/Temp'
-
+import OtpModal from './components/SiteModules/AuthModals/OtpModal'
+import ForgetModal from './components/SiteModules/AuthModals/ForgetModal'
+import ChangePassModal from './components/SiteModules/AuthModals/ChangePassModal'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +12,9 @@ function App() {
     <>
        <Routes>
         <Route path="/" element={ <Temp/> } />
+      <Route path='/otp' element={<OtpModal/>}/>
+      <Route path='/forget' element={<ForgetModal/>}/>
+      <Route path='/changePass' element={<ChangePassModal/>}/>
       </Routes>
     </>
   )
