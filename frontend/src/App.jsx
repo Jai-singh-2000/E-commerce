3c1a85a0da16e81d1e705bc904c460812d857d40
Jai singh <jaibhandari804@gmail.com>
2023-04-23 22:36:12 +0530

    login page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom"
+import { useState } from 'react'
 import Temp from './pages/Temp'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,9 @@ function App() {
     <>
        <Routes>
         <Route path="/" element={ <Temp/> } />
+        <Route path="/login" element={ <Login/> } />
+        <Route path="/signup" element={ <SignUp/> } />
+
       </Routes>
     </>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,107 @@
-import React from 'react'
+import React from 'react';
+import { Typography,Box,CardMedia,FormControl,IconButton,InputAdornment,InputLabel,OutlinedInput,Button} from '@mui/material';
+import loginImg from "../assets/Auth/login.jpg"
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {Link} from "react-router-dom"
 
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
-    <div>Login</div>
+    <Box display={'flex'} height={'100vh'}>
+
+      
+      <Box flex={0.5} flexDirection={'column'} display={'flex'} justifyContent={'space-evenly'}>
+
+        <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+          <Box>
+            <CardMedia
+            component="img"
+            height="300"
+            image={loginImg}
+            alt="Paella dish"
+          />
+          </Box>
+        </Box>
+        
+        <Box display={'flex'} justifyContent={'center'} flexDirection={'column'} alignItems={'center'}>
+          <Box  textAlign={'center'}>
+            <Typography sx={{fontSize:"2rem",fontWeight:"500",marginBottom:'1rem'}}>Login into your account</Typography>
+            <Typography sx={{fontWeight:'400',color:"gray"}}>Start your shopping with our website and get assured returns</Typography>
+            <Typography sx={{fontWeight:'400',color:"gray"}}>Manage your shopping faster than anyone, anywhere and anytime</Typography>
+          </Box>
+        </Box>
+      
+      </Box>
+
+
+      <Box bgcolor={'beige'} flex={0.5} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        
+        <Box  width={'55%'} bgcolor={'white'} padding={'3rem 2rem'} borderRadius={'10px'}>
+
+        <Box mb={'1rem'}>
+          <Typography sx={{fontSize:'1.8rem',fontWeight:'500'}}>Welcome Back !</Typography>
+          <Typography sx={{fontSize:'.9rem',color:'gray'}}>Continue your journey with us with lots of love</Typography>
+        </Box>
+
+        <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+          
+          <FormControl sx={{ m: 1 }} fullWidth variant="outlined" >
+            <InputLabel htmlFor="outlined-adornment-password">Email</InputLabel>
+            <OutlinedInput
+            sx={{width:"100%"}}
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}   
+              label="email"
+            />
+          </FormControl>
+            
+          <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+
+          <Button variant="contained" fullWidth sx={{ m: 1,textTransform:'capitalize',p:'.7rem 0' }}>Login</Button>
+
+        </Box>
+
+        <Box mt={'1rem'}>
+          <Typography textAlign={'center'}>Dont't you have an account ? <Typography component={'span'}>
+            <Link to={"/signup"} style={{ textDecoration: 'none' }}>
+            Sign Up
+            </Link>
+            </Typography></Typography>
+
+            <Typography mt={'.5rem'} textAlign={'center'} color={'red'}>Forgot Password ?</Typography>
+        </Box>
+        
+        </Box>
+
+      </Box>
+    
+    </Box>
   )
 }
 
