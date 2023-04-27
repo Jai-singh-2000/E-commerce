718c49f9271be450fdcd4a294418d9dd643a3fed
Jai singh <jaibhandari804@gmail.com>
2023-04-27 15:05:36 +0530

    product route created, nodemon added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express=require("express");
+const products=require("./data/product")
+const path=require("path")
 
 const app=express();
 const port=4000||port.env;
 
 app.get("/",(req,res)=>{
     res.send("<h5>Hello boi</h5>")
+})
+
+app.get("/products",(req,res)=>{
+    res.json(products)
 })
 
 
