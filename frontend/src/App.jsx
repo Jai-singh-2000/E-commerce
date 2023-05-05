29659ad4b45113bab67c80654f0a07394a8b8c68
suraj kumar <surajgautam56878@gmail.com>
2023-05-06 02:13:15 +0530

    first page done and 2nd page almost done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import OtpVerify from './pages/OtpVerify'
 import ChangePassword from './pages/ChangePassword'
+import LearnMore from './pages/LearnMore'
+import Product from './pages/Product'
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/signup" element={ <SignUp/> } />
         <Route path="/otp" element={ <OtpVerify/> } />
         <Route path="/change-password" element={ <ChangePassword/> } />
+        <Route path="/learnMore" element={ <LearnMore/> } />
+        <Route path="/product" element={ <Product/> } />
       </Routes>
     </>
   )
---
+++ frontend/src/pages/LearnMore.jsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from '@mui/material'
+import Button from '@mui/material/Button';
+import React from 'react'
+import OrganicImg from '../assets/Design/Organic.png';
+const LearnMore = () => {
+    const styles = {
+        background: `url(${OrganicImg}) no-repeat center center fixed`,
+        backgroundSize: "cover",
+        height: "100vh",
+        width: "100%",
+    };
+    return (
+        <>
+            <Box sx={styles}>
+                <Box display='flex'>
+                    <Box height='100vh' flex='0.5' > </Box>
+                    <Box bgcolor='#B22222' height='100vh' flex='0.5' display='flex'>
+                        <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' >
+
+                            <Typography display='flex' justifyContent='start' width="77%" fontFamily='sans-serif' fontWeight='700' fontSize="40px" color='white' paddingBottom="10px">All About Organic</Typography>
+
+                            <Typography color='white' paddingBottom="20px" fontWeight='200' paddingX={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim suhsah suha  aisa  sia sii isa  suhua veniam. </Typography>
+
+                            <Typography color='white' fontWeight='200' paddingX={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  </Typography>
+
+                            <Box width='76%' paddingTop='40px'>
+                                <Button variant="contained" style={{
+                                    borderRadius: 20,
+                                    backgroundColor: "#000000",
+                                    paddingInlineStart:"30px",
+                                    paddingInlineEnd:"30px"
+                                   
+                                }} width='400px' bgcolor='green' >Learn More</Button>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default LearnMore
