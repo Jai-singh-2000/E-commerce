45abb05b3615c68df1fd322caef7c56c5d5f7cfa
Jai singh <jaibhandari804@gmail.com>
2023-05-08 14:49:02 +0530

    connect mongo db to website

---
+++ backend/config/config.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+
+const connectDb=async()=>{
+    try{
+        const conn=await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Mongodb connected ${conn.connection.host}`)
+    }catch(error)
+    {
+        console.log("Server is not connecting to db ...",error.message)
+        process.exit(1); //Exit from this process 1 represents failure
+    }
+}
+
+
+
+module.exports=connectDb;
