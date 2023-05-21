c86d3bf890979e8cbe37eefa8474b50825d07a13
Jai singh <jaibhandari804@gmail.com>
2023-05-21 21:34:36 +0530

    feature- footer structure done

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Box from "@mui/material/Box";
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const Footer = () => {
   return (
@@ -24,10 +29,38 @@ const Footer = () => {
         <Typography  lineHeight={'2rem'}>Track My Order</Typography>
         <Typography  lineHeight={'2rem'}>Help</Typography>
       </Box>
-      <Box bgcolor={'pink'} flex='0.4'>
+      <Box flex='0.4'  color='#7D8EA7'>
         <Typography variant='h5' sx={{fontWeight:600,my:'1rem',color:'white'}}>Shop</Typography>
+        <Box>
+          <Typography lineHeight={'2rem'} fontSize={'.9rem'}><Typography component={'span'} fontWeight={600}>Address : </Typography>5143 Delhi Tilak Market 110042</Typography>
+        </Box>
+        <Box>
+          <Typography lineHeight={'2rem'} fontSize={'.9rem'}><Typography component={'span'} fontWeight={600}>Phone : </Typography>(+91) 145454548, (+91) 154545865</Typography>
+        </Box>
+        <Box>
+          <Typography lineHeight={'2rem'} fontSize={'.9rem'}><Typography component={'span'} fontWeight={600}>Hours : </Typography>10:00 - 15:00 Mon - Sat</Typography>
+        </Box>
+        
+        <Box mt='2rem'>
+          <IconButton sx={{color:'white'}}>
+            <FacebookIcon/>
+          </IconButton>
+          <IconButton  sx={{color:'white'}}>
+            <TwitterIcon/>
+          </IconButton>
+          <IconButton  sx={{color:'white'}}>
+            <InstagramIcon/>
+          </IconButton>
+          <IconButton  sx={{color:'white'}}>
+            <YouTubeIcon/>
+          </IconButton>
+          <IconButton  sx={{color:'white'}}>
+            <WhatsAppIcon/>
+          </IconButton>
+        </Box>
+
       </Box>
-      
+
       </Box>
     </Box>
   )
