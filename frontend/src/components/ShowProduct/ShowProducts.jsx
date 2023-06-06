f4bbfedb900307fbfb8e78d53d1f939239b88fc8
Jai singh <jaibhandari804@gmail.com>
2023-06-06 12:05:09 +0530

    add-single product api done

--- frontend/src/components/ShowProduct/ShowProducts.jsx
+++ frontend/src/components/ShowProduct/ShowProducts.jsx
@@ -1,17 +1,35 @@
-import React,{useState,useEffect} from 'react';
+import React,{useEffect} from 'react';
 import Header from '../Header/Header';
 import ShowProduct from './ShowProduct';
 import Products from '../Products/Products';
 import Footer from '../Footer/Footer';
 import { useSelector } from 'react-redux';
+import { fetchSingleProductApi } from '../../api/devApi';
+import { useParams } from 'react-router-dom';
 
 const ShowProducts = () => {
+  const {pid}=useParams()
   const {data:products}=useSelector((state)=>state?.products)
+
+  const fetchSingleProduct=async()=>{
+    try{
+      const response=await fetchSingleProductApi(pid);
+      console.log(response,"single ka response")
+    }catch(err)
+    {
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    fetchSingleProduct()
+  },[])
 
   return (
     <>
     <Header/>
-    <ShowProduct/>
+    {/* send single products state/props in show product comp */}
+    <ShowProduct/> 
     <Products products={products}/>
     <Footer/>
     </>
