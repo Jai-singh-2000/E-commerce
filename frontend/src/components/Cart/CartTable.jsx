e3cddc23e4cd6f8acfab09b46b709002b7860e3a
Jai singh <jaibhandari804@gmail.com>
2023-06-14 14:02:02 +0530

    redesign- cart table text

--- frontend/src/components/Cart/CartTable.jsx
+++ frontend/src/components/Cart/CartTable.jsx
@@ -19,7 +19,7 @@ const CartTable = () => {
     },[])
 
   return (
-    <Box flex={0.3} pr='1rem' height={'20rem'} position={'sticky'} top={'5rem'} boxSizing={'border-box'} mb='2rem'>
+    <Box flex={0.3} pr='3rem' height={'19rem'} position={'sticky'} top={'5rem'} boxSizing={'border-box'} mb='2rem'>
         <Box bgcolor={'white'} px={'1rem'} height={'100%'} display={'flex'} flexDirection={'column'}>
             
             <Box display='flex' justifyContent='center' alignItems={'center'} flex='0.15'>
@@ -29,27 +29,27 @@ const CartTable = () => {
             
             <Box flex='0.5' display={'flex'} flexDirection={'column'} justifyContent={'space-evenly'}>
             <Box display={'flex'} justifyContent={'space-between'}>
-                <Typography fontSize='1.15rem'>Price ({products?.length} item)</Typography>
-                <Typography fontSize='1.15rem'>₹ {totalPrice}</Typography>
+                <Typography fontSize='1.1rem'>Price ({products?.length} item)</Typography>
+                <Typography fontSize='1.1rem'>₹ {totalPrice}</Typography>
             </Box>
             <Box display={'flex'} justifyContent={'space-between'}>
-                <Typography fontSize='1.15rem'>Discount</Typography>
-                <Typography fontSize='1.15rem'>− ₹ 0</Typography>
+                <Typography fontSize='1.1rem'>Discount</Typography>
+                <Typography fontSize='1.1rem'>− ₹ 0</Typography>
             </Box>
             <Box display={'flex'} justifyContent={'space-between'}>
-                <Typography fontSize='1.15rem'>Delivery Charges</Typography>
-                <Typography fontSize='1.15rem'>Free</Typography>
+                <Typography fontSize='1.1rem'>Delivery Charges</Typography>
+                <Typography fontSize='1.1rem'>Free</Typography>
             </Box>
             </Box>
             <Divider/>
 
-            <Box flex='0.2' display={'flex'} justifyContent={'space-between'} alignItems={'center'} >
-                <Typography fontSize='1.15rem' fontWeight={'600'}>Total Amount</Typography>
-                <Typography fontSize='1.15rem' color='#388e3c' fontWeight={'600'}>₹ {totalPrice}</Typography>
+            <Box flex='0.18' display={'flex'} justifyContent={'space-between'} alignItems={'center'} >
+                <Typography fontSize='1.1rem' fontWeight={'600'}>Total Amount</Typography>
+                <Typography fontSize='1.1rem' color='#388e3c' fontWeight={'600'}>₹ {totalPrice}</Typography>
             </Box>
             <Divider/>
 
-            <Box flex='0.15' display={'flex'} alignItems={'center'}>
+            <Box flex='0.17' display={'flex'} alignItems={'center'}>
             <Typography color='#388e3c' fontWeight={600}>You will save ₹ 0 on this order</Typography>
             </Box>
         </Box>
