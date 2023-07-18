4b8afb72c048c313dcc4533ac0119aba1df06731
suraj kumar <surajgautam56878@gmail.com>
2023-07-18 11:39:04 +0530

    working in all order detailes page

---
+++ frontend/src/components/AllOrderDetailes/AllOrderDetailes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Typography, TextField, Button, Grid, Box } from '@mui/material';
+
+const AllOrderDetailes = () => {
+
+
+    return (
+        <>
+            <Grid container marginY={2}>
+               
+            </Grid>
+        </>
+    );
+};
+
+export default AllOrderDetailes;
