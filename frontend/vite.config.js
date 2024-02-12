01df94319cf0db3be3744b663dfae21353047116
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-02-12 14:12:21 +0530

    modify: axios base proxy url on vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-        '/api': {
-            target: 'https://planet-backend-92ic.onrender.com/api',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
-          },
+        '/api': `https://planet-backend-92ic.onrender.com`
     },
   },
 })
