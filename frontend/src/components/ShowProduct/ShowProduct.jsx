357b1b8a08e2e9f45aff22e7b4574ef0e6de92e5
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-02-19 17:39:38 +0530

    add: skeleton on product show

--- frontend/src/components/ShowProduct/ShowProduct.jsx
+++ frontend/src/components/ShowProduct/ShowProduct.jsx
@@ -1,84 +1,167 @@
 import React, { useEffect, useState } from 'react';
-import { Box,CardMedia,Typography, Button,FormControl,MenuItem,Select,InputLabel } from '@mui/material';
+import { Box, CardMedia, Typography, Button, FormControl, MenuItem, Select, InputLabel } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { addToCartAsync } from '../../redux/reducers/cartSlice';
 import { useNavigate } from 'react-router-dom';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
 
-const ShowProduct = ({obj}) => {
-    const dispatch=useDispatch()
-    const navigate=useNavigate()
-    const [qty,setQty]=useState(0);
 
-    const handleCart=()=>{
-        const dummy={
-            id:obj?._id,
-            qty:qty
+const ShowProduct = ({ obj }) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [qty, setQty] = useState(0);
+
+    const handleCart = () => {
+        const dummy = {
+            id: obj?._id,
+            qty: qty
         }
         dispatch(addToCartAsync(dummy))
         navigate("/cart")
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setQty(1)
-    },[obj])
+    }, [obj])
 
-  return (
-    <>
-        <Box display={'flex'} height={'80vh'}>
-            <Box flex={0.4} display={'flex'} justifyContent={'center'} alignItems={'flex-end'}>
-                <CardMedia
-                    sx={{ height: '95%',width:'90%',borderRadius:'20px',objectFit:"contain" }}
-                    image={obj?.image||"https://images.pexels.com/photos/2850487/pexels-photo-2850487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-                    title="green iguana"
-                />
-            </Box>
-            <Box flex={0.6} display={'flex'} flexDirection={'column'} boxSizing={'border-box'} p='2rem'  justifyContent={'space-around'}>
+    return (
+        <>
+            <Box display={'flex'} height={'80vh'}>
+                <Box flex={0.4} display={'flex'} justifyContent={'center'} alignItems={'flex-end'}>
 
-                <Box flex='0.6' display={'flex'} flexDirection={'column'} justifyContent={'space-evenly'}>                
-                    <Typography fontSize={'1rem'}>{obj?.brand} ({obj?.category})</Typography>
-                    <Typography fontSize={'1.5rem'}>{obj?.name}</Typography>
-                    <Typography fontSize={'28px'}>₹{Math.floor(obj?.totalPrice)} <Typography as='del' fontSize='1rem' sx={{color:"grey",fontWeight:400}}>{obj?.price&&`₹${obj?.price}`}</Typography> <Typography as='span' fontSize='1rem' color='#388E3C'>{obj?.discount&&`${obj?.discount}% off`}</Typography> </Typography>
-                    <Box py='1rem' display={'flex'} alignItems={'center'}>
-                        <Box>
-                        <FormControl fullWidth  size="small" sx={{ m: 1, minWidth: 60 }}>
-                        <InputLabel id="demo-simple-select-label">Qty</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={qty}
-                            label="Age"
-                            onChange={(e)=>setQty(e.target.value)}
-                        >
+                    {
+                        obj?.image ? (
+                            <CardMedia
+                                sx={{ height: '95%', width: '90%', borderRadius: '20px', objectFit: "contain" }}
+                                image={obj?.image}
+                                title="green iguana"
+                            />
+                        ) : (
+                            <ImageSkeleton />
+                        )
+                    }
+                </Box>
+                <Box flex={0.6} display={'flex'} flexDirection={'column'} boxSizing={'border-box'} p='2rem' justifyContent={'space-around'}>
+
+                    <Box flex='0.6' display={'flex'} flexDirection={'column'} justifyContent={'space-evenly'}>
+
+                        <Typography fontSize={'1rem'}>
                             {
-                                [...Array(obj?.countInStock).keys()].map((item)=>{
-                                    return <MenuItem key={item} value={item+1}>{item+1}</MenuItem>
-                                })
+                                obj?.brand ? (
+                                    `${obj?.brand} ${obj?.category}`
+                                ) : (
+                                    <Skeleton />
+                                )
                             }
-                           
-                        </Select>
-                        </FormControl>
-                        </Box>
-                    </Box>
-                    <Box fontSize={'1.5rem'}>
-                        <Button variant='contained' sx={{marginRight:'1rem',bgcolor:"#DB4444",px:"3rem",py:"1rem",
-                        "&:hover":{
-                            bgcolor:"#d63e3e",
-                            color:"white"
-                        }
-                    }} onClick={handleCart}>Add to Cart</Button>
-                        {/* <Button variant='contained' sx={{bgcolor:"#3CB815"}}>Buy Now</Button> */}
-                    </Box>
-                    
-                </Box>
+                        </Typography>
 
-                <Box flex='0.38'>
-                    <Typography variant='h5'>Description</Typography>
-                    <Typography py='1rem'>{obj?.description}</Typography>
+                        <Typography fontSize={'1.5rem'}>
+                            {
+                                obj?.name ? obj.name : <Skeleton />
+                            }
+                        </Typography>
+
+                        <Typography fontSize={'28px'} color={'green'}>
+                            {
+                                obj?.totalPrice ? (
+                                    <Box>
+                                        ₹{Math.floor(obj?.totalPrice)}{" "}
+                                        <Typography as='del' fontSize='1rem' sx={{ color: "grey", fontWeight: 400 }}>
+                                            {obj?.price && `₹${obj?.price}`}
+                                        </Typography>
+
+                                        <Typography as='span' fontSize='1rem' color='#388E3C'>{obj?.discount && `${obj?.discount}% off`}</Typography>
+                                    </Box>
+                                ) : (
+                                    <Skeleton />
+                                )
+                            }
+
+                        </Typography>
+
+                        <Box py='1rem' display={'flex'} alignItems={'center'}>
+                            <Box>
+                                {
+                                    obj?.countInStock ? (
+                                        <FormControl fullWidth size="small" sx={{ m: 1, minWidth: 60 }}>
+
+                                            <InputLabel id="demo-simple-select-label">Qty</InputLabel>
+
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                value={qty}
+                                                label="Age"
+                                                onChange={(e) => setQty(e.target.value)}
+                                            >
+                                                {
+                                                    [...Array(obj?.countInStock).keys()].map((item) => {
+                                                        return <MenuItem key={item} value={item + 1}>{item + 1}</MenuItem>
+                                                    })
+                                                }
+
+                                            </Select>
+                                        </FormControl>
+                                    ) : (
+                                        <Skeleton sx={{ p: 2, minWidth: 60 }} />
+                                    )
+                                }
+
+                            </Box>
+                        </Box>
+                        <Box fontSize={'1.5rem'}>
+                            {
+                                obj?.name ? (
+                                    <Button variant='contained' sx={{
+                                        marginRight: '1rem', bgcolor: "#0D9276", px: "3rem", py: "1rem",
+                                        "&:hover": {
+                                            bgcolor: "#047d63",
+                                            color: "white"
+                                        }
+                                    }} onClick={handleCart}>Add to Cart</Button>
+                                ) : (
+                                    <Skeleton sx={{ px: "3rem", py: "1.5rem", width: "6rem" }} />
+                                )
+                            }
+
+                        </Box>
+
+                    </Box>
+
+                    <Box flex='0.38'>
+                        {
+                            obj?.name ?
+                                (
+                                    <Typography variant='h5'>
+                                        Description
+                                    </Typography>
+                                )
+                                : <Skeleton sx={{ px: "3rem", py: ".3rem", width: "6rem" }} />
+                        }
+                        {
+                            obj?.description ? (
+                                <Typography py='1rem'>
+                                    {obj?.description}
+                                </Typography>
+                            ) : (
+                                <Skeleton width={'100%'} height={'8rem'} />
+                            )
+                        }
+                    </Box>
                 </Box>
             </Box>
-        </Box>
-    </>
-  )
+        </>
+    )
 }
 
 export default ShowProduct
+
+
+export function ImageSkeleton() {
+    return (
+        <Stack spacing={1} sx={{ height: '95%', width: '80%', borderRadius: '20px', objectFit: "contain" }} >
+            <Skeleton variant="rounded" width={'100%'} height={'100%'} />
+        </Stack>
+    );
+}
