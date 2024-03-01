6aa20c32faa60d27637f0e1a9348e5e8d2263a05
Jai Singh <100370894+Jai-singh-2000@users.noreply.github.com>
2024-03-01 14:50:53 +0530

    remove: comment and unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,50 +24,15 @@ import Error from "./components/Tools/Error";
 import Orders from "./pages/Orders";
 import Shop from "./pages/Shop";
 import ContactEmail from "./pages/ContactEmail";
-import EditProduct from "./components/Admin/EditProduct";
-// import Loader from "./components/Tools/Loader";
-// import ProtectedRoute from "./components/Provider/ProtectedRoute";
-// import { tokenVerificationAsync } from "./redux/reducers/userSlice";
-// import { useNavigate } from "react-router-dom";
-// import { getToken, getAdmin } from "./utils/functions";
+  import EditProduct from "./components/Admin/EditProduct";
 
 function App() {
   const isAdminLogged = useSelector((state) => state?.user?.isAdminLogged);
   const dispatch = useDispatch();
-  // const [loading, setLoading] = useState(true);
-  // const navigate = useNavigate();
-
-  // const checkPath = () => {
-  //   let path = location.pathname;
-  //   const token = getToken();
-  //   const admin = getAdmin();
-  //   if (token) {
-
-  //     if (admin && (path === '/' || path === "/cart" || path === "/about" || path === '/profile' || path === '/contact' || path === "/shipping" || path === '/order' || path === '/payment' || path.slice(0, 9) === '/product/')) {
-  //       navigate("/dashboard")
-  //     }
-  //     else if (path === "/login" || path === "/signup") {
-  //       navigate("/");
-  //     } else {
-  //       navigate(path);
-  //     }
-
-  //   }
-  //   else {
-  //     if (path === "/login" || path === "/signup" || path === "/shop" || path === "/about" || path === "/contact" || path === "/cart") {
-  //       navigate(path);
-  //     } else {
-  //       navigate("/");
-  //     }
-  //   }
-  //   setLoading(false)
-  // };
 
   useEffect(() => {
     dispatch(fetchAllProducts());
   }, []);
-
-
 
 
   return (
